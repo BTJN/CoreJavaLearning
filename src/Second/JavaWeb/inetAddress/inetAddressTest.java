0e6c6832720e19ee85ddb34fdd93f3f4295556b4
BTJN <2159346546@qq.com>
2025-08-29 18:02:18 +0800

    打印链接host地址的主机名。

---
+++ src/Second/JavaWeb/inetAddress/inetAddressTest.java
@@ -0,0 +1,23 @@
+package Second.JavaWeb.inetAddress;
+
+import java.io.*;
+import java.net.*;
+
+public class inetAddressTest {
+
+    public static void main(String[] args) throws IOException{
+        if (args.length > 0)
+        {
+            String host = args[0];
+            InetAddress[] addresses = InetAddress.getAllByName(host);
+            for (var addr : addresses)
+                System.out.println(addr);
+        }
+        else
+        {
+            InetAddress localhostAddress = InetAddress.getLocalHost();
+            System.out.println(localhostAddress);
+        }
+    }
+
+}
