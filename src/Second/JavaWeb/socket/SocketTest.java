9d16fd74415e325a1d92bfd9b78809820c0bfef3
BTJN <2159346546@qq.com>
2025-08-29 13:33:36 +0800

    使用socket套接字链接程序内部和外部网络通信

---
+++ src/Second/JavaWeb/socket/SocketTest.java
@@ -0,0 +1,22 @@
+package Second.JavaWeb.socket;
+
+import java.io.*;
+import java.net.*;
+import java.nio.charset.*;
+import java.util.*;
+
+public class SocketTest {
+
+    public static void main(String[] args) throws Exception{
+        try (var socket = new Socket("time-a.nist.gov", 13);
+            var in = new Scanner(socket.getInputStream(), StandardCharsets.UTF_8);)
+        {
+            while (in.hasNextLine())
+            {
+                String Line = in.nextLine();
+                System.out.println(Line);
+            }
+        }
+    }
+
+}
